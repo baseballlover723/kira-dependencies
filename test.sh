f95e0eddf8ac4daf26be29991f7a25425fa5027b
Philip Ross <philipr@synopsys.com>
2021-02-27 16:56:41 -0800

    REMOVE testing setup

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+repo=$1
+pm=$2
+dir=$3
+tag=kira-bot/tinfoil/${repo}${dir:+/$dir}:${pm}
+
+echo "tag: $tag"
+
+DOCKER_BUILDKIT=0 docker build --pull --tag $tag . --build-arg PROJECT=$repo --build-arg PACKAGE_MANAGER_ARG=$pm --build-arg DIR=${dir} --build-arg KIRA_GITHUB_PERSONAL_TOKEN_ARG=$KIRA_GITHUB_PERSONAL_TOKEN --build-arg KIRA_GITLAB_PERSONAL_TOKEN_ARG=$KIRA_GITLAB_PERSONAL_TOKEN
+time docker run -it --rm $tag
+docker rmi $tag
